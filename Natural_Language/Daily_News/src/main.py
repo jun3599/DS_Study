f066f6fc933d95871dca51cb6976f53cbaa92535
JunHwi Park <wnsgnl3599@naver.com>
2022-02-21 20:13:27 +0900

    add small diffs

--- Natural_Language/Daily_News/src/main.py
+++ Natural_Language/Daily_News/src/main.py
@@ -4,18 +4,28 @@ from text_manager import *
 import pandas as pd 
 
 
-News = pd.read_excel('C:\\Users\\wnsgn\\Desktop\\pro_git\\data_science_study\\DS_Study\\ars_praxia_result.xlsx')
+# # News = pd.read_excel('C:\\Users\\wnsgn\\Desktop\\pro_git\\data_science_study\\DS_Study\\ars_praxia_result.xlsx')
 
-t = text_Analyzer(News, 'article')
-t.draw_word_cloud()
-t.latent_Dirichlet_allocation()
-t.draw_n_nodes_network()
+# # t = text_Analyzer(News, 'article')
+# # t.draw_word_cloud()
+# # t.latent_Dirichlet_allocation()
+# # t.draw_n_nodes_network()
 
 
-# if __name__ == '__main__' :
-#     keyword = input("검색 키워드를 입력해주세요: ")
-#     result = scrap_news(search_keywords='아르스 프락시아')
-#     t = text_Analyzer(result, 'article')
-#     t.draw_word_cloud()
-#     t.latent_Dirichlet_allocation()
-#     t.draw_n_nodes_network()
+if __name__ == '__main__' :
+    # keyword = input("검색 키워드를 입력해주세요: ")
+    result = scrap_news(search_keywords='mode=LSD&mid=sec&sid1=101&listType=paper&date=20220221')
+    # t = text_Analyzer(result, 'article')
+    # t.draw_word_cloud()
+    # t.latent_Dirichlet_allocation()
+    # t.draw_n_nodes_network()
+    print(result)
+    result.to_excel('./test_scrap_0221.xlsx')
+
+# # https://news.naver.com/main/list.naver?mode=LSD&mid=sec&sid1=001
+# 'https://news.naver.com/main/list.naver?mode=LSD&mid=sec&sid1=101&listType=paper&date=20220221'
+
+import queue
+from urllib.parse import quote 
+
+# print(quote('list.naver?mode=LSD&mid=sec&sid1=101&listType=paper&date=20220221'))
